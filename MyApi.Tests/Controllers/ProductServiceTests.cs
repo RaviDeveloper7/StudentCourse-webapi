26cb6964eac9a0bf597ea34d42d4c7e944a16aec
Ravi Shankar Yadav Durgam <rdurgam@pkrmsezdc.com>
2025-05-18 18:39:02 +0530

    FirstTestCaseAdded

--- MyApi.Tests/Controllers/ProductServiceTests.cs
+++ MyApi.Tests/Controllers/ProductServiceTests.cs
@@ -44,7 +44,7 @@ namespace MyApi.Tests.Controllers
             var productDTOs = new List<ProductReadDto>
             {
             new ProductReadDto { Id = 1, Name = "Item 1", Price = 10 },
-            new ProductReadDto { Id = 2, Name = "Item 2", Price = 20 }
+            new ProductReadDto { Id = 2, Name = "Item 2", Price = 200 }
             };
 
             _mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(products);
@@ -55,9 +55,27 @@ namespace MyApi.Tests.Controllers
             Assert.NotNull(result);
             Assert.Collection(result,
              item => Assert.Equal("Item 1", item.Name),
-             item => Assert.Equal("Item 2", item.Name)
+             item => Assert.Equal(10, item.Price),
+             item => Assert.Equal("Item 2", item.Name),
+             item => Assert.Equal(20, item.Price)
              );
         }
 
+        [Fact]
+        public async Task GetProductByIdAsync_ValidId_ReturnsMappedDTO()
+        {
+            var product = new Product { Id = 1, Name = "Ash", Price = 10 };
+
+            var productDTO = new ProductReadDto { Id = 1, Name = "Ash", Price = 10 };
+
+            _mockRepo.Setup(r=> r.GetByIdAsync(1)).ReturnsAsync(product);
+            _mockMapper.Setup(m => m.Map<ProductReadDto>(product)).Returns(productDTO);
+
+            var result = await _service.GetByIdAsync(1);
+
+            Assert.NotNull(result);
+            Assert.Equal("Ash", result.Name);
+
+        }
     }
 }
